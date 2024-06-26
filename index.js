1f46aa5485cced86d44ce4df4fa1d919b4bb0a50
NehemiasS <melkysx@gmail.com>
2024-06-25 23:40:57 -0600

    25062024
    errores de sintaxis resueltos

--- index.js
+++ index.js
@@ -2,19 +2,39 @@ document.addEventListener('DOMContentLoaded',function(){
     const title = document.getElementById('title');
     const description = document.getElementById('description');
     const table = document.getElementById('table');
+    const alert = document.getElementById('alert');
     const btn = document.getElementById('add');
 
-    btn.onclick = function(){
-        console.log('title:', title.value);
-        console.log('description:', description.value);
-    };
+    // btn.onclick = function(){
+    //     console.log('title:', title.value);
+    //     console.log('description:', description.value);
+    // };
 
     function addTodo() {
         if ( title.value === '' || description.value === ''){
-            console.error(' Titulo y descripcion requeridos');
-        }else{
-            console.log('todo bien');
+            // console.error(' Titulo y descripcion requeridos');
+            alert.classList.remove('d-none');
+            alert.innerText = 'Titulo y descripcion requeridos';
+            return;
         }
-    };
+        alert.classList.add('d-none');
+        const row = table.insertRow();
+        row.innerHTML = `
+        <td>${title.value}</td>
+        <td>${description.value}</td>
+        <td class="text-center">
+                <input type="checkbox">
+              </td>
+              <td class="text-right">
+                <button class="btn btn-primary mb-1">
+                  <i class="fa fa-pencil"></i>
+                </button>
+                <button class="btn btn-danger mb-1 ml-1">
+                  <i class="fa fa-trash"></i>
+                </button>
+              </td>
+        `; 
+    }
 
-})
+    btn.onclick = addTodo;
+});
